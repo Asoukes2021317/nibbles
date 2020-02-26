6c2f23b5f4e6c17909e545db08548a3992ad3208
Asoukes2021317 <60993209+Asoukes2021317@users.noreply.github.com>
2020-02-26 19:41:55 +1300

    Closer
    Main game loop updates, checks self collision, renamed Segment class to more appropriate Square class, when fruit needs to "teleport", it properly checks that it won't go onto snake, main game loop put into its own function, will be relevant later

--- test6.py
+++ test6.py
@@ -1,3 +1,5 @@
+# This code is primarily based on my JS pacman game from highschool and https://python-forum.io/Thread-PyGame-Basic-Snake-game-using-OOP (mainly for implementation of snake/segment classes)
+
 import time
 import random
 import pygame
@@ -5,21 +7,20 @@ pygame.init()
 
 cellSize = 30
 
-screenDim = [20, 15]
+screenDim = [7, 7]
 size = (screenDim[0]*cellSize, screenDim[1]*cellSize)
 screen = pygame.display.set_mode(size)
 
 carryOn = True
-direction = 0 # up
+direction = 3 # left
 clock = pygame.time.Clock()
 points = 0
 
-class Segment:
+class Square:
     def __init__(self, x, y, colour):
         self.x = x
         self.y = y
         self.colour = colour
-
 
 class Snake:
     def __init__(self, length, colour):
@@ -28,19 +29,26 @@ class Snake:
         self.colour = colour
         self.pause = 300
         self.turn = 0
-        x = 7
-        y = 7
+        x = int(screenDim[0]/2)
+        y = int(screenDim[1]/2)
         for seg in range(length):
-            self.segments.append(Segment(x, y, colour))
-            x += 1
+            self.segments.append(Square(x, y, colour))
+            x += 1 # add segments to the right of "head"
 
     def isValid(self, theX, theY):
-        return bool(theX >= 0 and theX < screenDim[0] and theY >= 0 and theY < screenDim[1])
+        if theX >= 0 and theX < screenDim[0] and theY >= 0 and theY < screenDim[1]:
+            for seggy in range(1, len(self.segments), 1):
+                if theX == self.segments[seggy].x and theY == self.segments[seggy].y:
+                    return False
+            return True
+        else:
+            return False
 
     def movePlayer(self):
         if self.turn < now():
             # checkKeys()
             self.turn = now()+self.pause
+
             for segment in range(len(self.segments)-1, 0, -1): # look at the segments in reverse order
                 self.segments[segment].x = self.segments[segment-1].x
                 self.segments[segment].y = self.segments[segment-1].y
@@ -57,24 +65,32 @@ class Snake:
                 print("Game OVER!!!")
 
             if self.segments[0].x == apple.x and self.segments[0].y == apple.y:
-                moveFruit()
+                moveFruit(self)
 
     def ateFruit(self):
         global points
         lastSegment = len(self.segments)-1
-        self.segments.append(Segment(self.segments[lastSegment].x, self.segments[lastSegment].y, self.colour))
+        self.segments.append(Square(self.segments[lastSegment].x, self.segments[lastSegment].y, self.colour))
         points += 1
         print("Your points:", points)
 
 #class Player:
 player = Snake(4, (160, 0, 160))
-apple = Segment(1, 1, (0, 200, 0))
+apple = Square(1, 1, (0, 200, 0))
 
-def moveFruit():
+def moveFruit(self):
     pos = [apple.x, apple.y]
-    for segment in player.segments:
-        while pos[0] == segment.x and pos[1] == segment.y:
-            pos = [random.randint(0, screenDim[0]-1), random.randint(0, screenDim[1]-1)]
+    done = False
+    while not done:
+        for segment in self.segments:
+            while pos[0] == segment.x and pos[1] == segment.y:
+                pos = [random.randint(0, screenDim[0]-1), random.randint(0, screenDim[1]-1)]
+                hasRandomised = True
+        if hasRandomised:
+            hasRandomised = False # reset to go through all the segments again
+        else:
+            done = True
+
     player.ateFruit()
     apple.x = pos[0]
     apple.y = pos[1]
@@ -102,18 +118,30 @@ def checkKeys():
     if keys[pygame.K_LEFT]:
         direction = 3
 
+def mainLoop():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+        checkKeys()
+        #moveFruit()
+        player.movePlayer()
+        renderScreen()
 
-    checkKeys()
-    #moveFruit()
-    player.movePlayer()
-    renderScreen()
+        clock.tick(60)
 
-    clock.tick(60)
+#def end():
+#stuff...
+
+# - - - - - | Actually Start The Game | - - - - - #
+# - - - | Intro Set Go | - - - #
+# welcome() allow user to choose screen size, greeting pic
+# - - - | While Go | - - - #
+# while going:
+# readyUp() # get user to press enter when ready, maybe not need this fxn
+mainLoop()
+# end() ask to if want try again or not
 
 pygame.quit()
